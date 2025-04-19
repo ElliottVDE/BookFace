b9b6a88c6d2559b8fc3c04970ce5fa1b9dafd35d
ElliottVDE <116677046+ElliottVDE@users.noreply.github.com>
2025-04-18 21:21:27 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from"express";
 import cors from "cors";
-import mongoose from "mongoose";
 import posts from "./client/server/routes/post.js";
 import users from "./client/server/routes/user.js";
 import groups from "./client/server/routes/groups.js";
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT;
 const app = express();
-// const groupRoutes = require('./server/routes/groups');
-
 
 
 app.use(cors());
@@ -16,32 +13,6 @@ app.use("/post", posts);
 app.use("/user", users);
 app.use("/groups", groups);
 
-
-// media routes
-// app.get("/", (req, res) => {
-//     const body = req.body;
-//     try {
-//         Post.find({}).then(data => {
-//             res.json(data)
-//         }).catch(error => {
-//             res.status(408).json({ error })
-//         })
-//     } catch (error) {
-
-//     } {
-//         res.status(409).json({ msg: error.message })
-//     }
-// })
-// app.post("/uploads", async (req, res) => {
-//     const body = req.body;
-//     try {
-//         const newImage = await Post.create(body);
-//         newImage.save();
-//         res.status(201).json({ msg: "New Image uploaded..."})
-//     } catch (error) {
-//         res.status(409).json({ msg: error.message })
-//     }
-// })
 
 // Start the express server
 app.listen(PORT, () => {
