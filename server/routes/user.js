6b560742bfc7015494cecc8564abe5de7a0d66d3
ElliottVDE <116677046+ElliottVDE@users.noreply.github.com>
2025-04-20 04:24:10 -0400

    Add files via upload

--- server/routes/user.js
+++ server/routes/user.js
@@ -81,7 +81,8 @@ router.post("/login", async (req, res) => {
         if (!isPasswordCorrect) {
             return res.status(401).json({message: "Invalid username or password"});
         } else {
-           return res.send({ message: 'Login successful!' });
+           return res.status(200).json({ message: 'Login successful!', user: { username: user.username, email: user.email }
+        });
         }
 
         // Create a JWT token (Optional: use your secret key in place of 'your_jwt_secret')
